85eb8c86aa60ba8adef8808ec236100dd61918b2
8884727888 <53405109+8884727888@users.noreply.github.com>
2020-04-22 22:36:20 +0530

    Add files via upload

---
+++ Code_Histogram.py
@@ -0,0 +1,9 @@
+### Histograms are used to represent data given in form of some groups. X-axis is bin ranges where Y-axis is frequency. you want to represent age wise population in form of graph ? 
+##then histogram suits well as it tells you how many exists in certain group range i.e. bin.
+import pandas as pd
+import matplotlib.pyplot as plt
+X=pd.read_csv("sample.csv")
+Xp=X["age"]
+plt.hist(Xp,bins=20)
+plt.show()
+###### we may have more observations for a accurate graph
